0a6af02e4ba3264df5944c03a1f40ce905bb1e00
kenxsak <kenxsak@gmail.com>
2025-12-30 21:44:12 +0530

    cahnges

--- src/components/tasks/add-task-dialog.tsx
+++ src/components/tasks/add-task-dialog.tsx
@@ -151,7 +151,7 @@ export default function AddTaskDialog({ isOpen, onOpenChange, onTaskSaved, taskT
         </DialogHeader>
         
         <form onSubmit={handleSubmit(onSubmit)}>
-          <DialogBody className="space-y-5">
+          <DialogBody className="space-y-5 max-h-[60vh] overflow-y-auto">
             <input type="hidden" {...control.register("companyId")} />
             
             {/* Title */}
